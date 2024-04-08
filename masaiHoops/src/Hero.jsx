c3bfca6f1ef79b1f359cc85d3fcbac27a3986533
Cole Stotland <colestotland10@gmail.com>
2024-04-08 12:42:04 -0400

    Update Hero.jsx

--- masaiHoops/src/Hero.jsx
+++ masaiHoops/src/Hero.jsx
@@ -1,15 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import Sphere from './Sphere';
 import { Canvas } from "@react-three/fiber"
+import { useFrame } from '@react-three/fiber';
 
 const Cube = ({position, size, color}) => {
+
+    const ref = useRef() 
+    useFrame((state, delta) => {
+      ref.current.rotation.x += delta;
+      ref.current.rotation.y += delta;
+      ref.current.rotation.z += delta;
+
+      ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
+
+      
+
+    })
     return ( 
-      <mesh position={position}>
+      <mesh position={position} ref={ref}>
         <boxGeometry args={size}/>
         <meshStandardMaterial color={color}/> 
       </mesh>
     )
+}
+
+
+const Sphere = ({position, size, color}) => {
+
+  const ref = useRef() 
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  useFrame((state, delta) => {
+    ref.current.rotation.y += delta * 0.2
+  })
+
+  return ( 
+    <mesh position={position} ref={ref}>
+      <sphereGeometry args={size}/>
+      <meshStandardMaterial color={color} wireframe/> 
+    </mesh>
+  )
+
+
+
 }
 
 function Hero() {
@@ -19,12 +53,16 @@ function Hero() {
       <directionalLight position={[2,2,2]}/>
       <ambientLight intensity={0.1} />
 
-      <group position={[0, 1, 0]}>
+      {/* <group position={[0, 1, 0]}>
         <Cube position={[1,-1,0]} color={"green"} size={[1,1,1]}/>
         <Cube position={[-1,-1,0]} color={"red"} size={[1,1,1]}/>
         <Cube position={[-1,1,0]} color={"yellow"} size={[1,1,1]}/>
         <Cube position={[1,1,0]} color={"blue"} size={[1,1,1]}/>
-      </group> 
+      </group>  */}
+
+      {/* <Cube position={[0, 0, 0]} size={[1,1,1]} color={"orange"} />  */}
+
+      <Sphere position={[0,0,0]} color={"orange"}/> 
 
     </Canvas>
   );
