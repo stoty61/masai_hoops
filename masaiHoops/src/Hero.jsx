e481a7e578736b18efbd50c543b99e3347a9f286
Cole Stotland <colestotland10@gmail.com>
2024-05-20 20:37:21 -0400

    progress?

--- masaiHoops/src/Hero.jsx
+++ masaiHoops/src/Hero.jsx
@@ -1,163 +1,150 @@
 import React, { useState, useEffect, useRef, Suspense } from 'react';
 import * as THREE from 'three';
-import { Canvas } from "@react-three/fiber"
+import { Canvas } from "@react-three/fiber";
 import { useFrame } from '@react-three/fiber';
-import { OrbitControls, useHelper, Stars, Html, Text3D, useGLTF  } from "@react-three/drei"
-import { useControls } from "leva"
-import { useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls, Stars, Text3D, useGLTF } from "@react-three/drei";
+import { useControls } from "leva";
 import logo from './assets/name.png';
 import kawhi from './kawhi-the-shot.gif';
 import Card from './Card';
 import fontJson from './fonts/cool.json'; // Path to your 3D font
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 
-const Cube = ({position, size, color}) => {
-
-    const ref = useRef() 
-    useFrame((state, delta) => {
-      ref.current.rotation.x += delta;
-      ref.current.rotation.y += delta;
-      ref.current.rotation.z += delta;
-
-      ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
-
-      
-
-    })
-    return ( 
-      <mesh position={position} ref={ref}>
-        <boxGeometry args={size}/>
-        <meshStandardMaterial color={color}/> 
-      </mesh>
-    )
-}
+const Cube = ({ position, size, color }) => {
+  const ref = useRef();
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta;
+    ref.current.rotation.z += delta;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
+  });
+  return (
+    <mesh position={position} ref={ref}>
+      <boxGeometry args={size} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
 
 function Earth() {
   const gltf = useGLTF('src/assets/earth.glb');
   const meshRef = useRef();
+  useEffect(() => {
+    // Disable receiving shadows for the Earth material
+    gltf.scene.traverse((child) => {
+      if (child.isMesh) {
+        child.receiveShadow = false; // Ensure shadows are not received
+        child.material.receiveShadow = false;
+        child.material.needsUpdate = true;
+      }
+    });
+  }, [gltf]);
   return <primitive object={gltf.scene} scale={[1.1, 1.1, 1.1]} position={[20, -1.2, 0]} ref={meshRef} />;
 }
+
 
 const Basketball = () => {
   const gltf = useGLTF('src/assets/Basketball_ball2.glb');
   const meshRef = useRef();
   const [isHovered, setIsHovered] = useState(false);
   useFrame((state, delta) => {
-      meshRef.current.rotation.y += delta * 1.15;
-      meshRef.current.rotation.y += isHovered ? 0.02 : 0;
+    meshRef.current.rotation.y += delta * 1.15;
+    meshRef.current.rotation.y += isHovered ? 0.02 : 0;
   });
-  return <primitive 
-      object={gltf.scene} 
-      scale={[10,10,10]}
+  return (
+    <primitive
+      object={gltf.scene}
+      scale={[10, 10, 10]}
       ref={meshRef}
-      position={[0,-2.5, 0]}
+      position={[0, -2.5, 0]}
       onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
       onPointerLeave={() => setIsHovered(false)}
-  />;
+    />
+  );
 };
 
-
-
-
-
 const Scene = () => {
+  const earthLightRef = useRef();
+  const ballLightRef = useRef();
+  const ballLightRef2 = useRef();
+  const ambientLightRef = useRef();
 
-  const directionalLightRef = useRef()
-
-  const {dLightColor, dLightIntensity} = useControls({
-    dLightColor: "orange",
-    dLightIntensity: {
-      value: 1,
+  const { earthLightIntensity, ballLightIntensity } = useControls({
+    earthLightIntensity: {
+      value: 10,
       min: 0,
-      max: 2
-    }
-  })
-
-  const ambientLightRef = useRef()
-
-  const {aLightColor, aLightIntensity} = useControls({
-    aLightColor: "orange",
-    aLightIntensity: {
-      value: 0.5,
+      max: 20,
+    },
+    ballLightIntensity: {
+      value: 10,
       min: 0,
-      max: 5
-    }
-  })
-
-
-  // useHelper(directionalLightRef, THREE.DirectionalLightHelper, 0.5, "black")
-
+      max: 20,
+    },
+  });
 
   return (
     <>
-      <directionalLight 
-      position={[-2,-2,-2]}
-      intensity={dLightIntensity}
-      ref={directionalLightRef}
-      color={dLightColor}/>
+      {/* Light for illuminating the Earth */}
+      <directionalLight
+        position={[-10, -10, -10]}
+        intensity={earthLightIntensity}
+        ref={earthLightRef}
+        color={"white"} // Set light color to white
+      />
+      {/* Light for illuminating the basketball */}
+      <directionalLight
+        position={[10, 10, 10]}
+        intensity={ballLightIntensity}
+        ref={ballLightRef}
+        color={"orange"} // Set light color to orange
+      />
 
-<directionalLight 
-      position={[2,2,2]}
-      intensity={dLightIntensity}
-      ref={directionalLightRef}
-      color={dLightColor}/>   
-      
-      <ambientLight intensity={aLightIntensity} color={aLightColor}
-      ref={ambientLightRef}/>
+            {/* Light for illuminating the basketball */}
+        <directionalLight
+        position={[-10, -10, -10]}
+        intensity={ballLightIntensity}
+        ref={ballLightRef2}
+        color={"orange"} // Set light color to orange
+      />
 
-      {/* <Sphere position={[0,0,0]} color={"orange"}/>  */}
-    
+
+      {/* Ambient light */}
+      <ambientLight intensity={0.5} color={"orange"} ref={ambientLightRef} />
       <Basketball />
-
-          {/* Add the Earth model to the right of the basketball */}
-      <Earth /> 
-
+      <Earth />
       <Text3D
-              font={fontJson} // Load the 3D font
-              size={1}        // Adjust size
-              height={0.5}    // Extrusion depth
-              bevelEnabled={true} // Add a bevel for a smoother look
-              bevelThickness={0.1} // Bevel thickness
-              bevelSize={0.1}      // Bevel size
-              bevelOffset={0}      // Bevel offset
-              bevelSegments={8}    // Number of bevel segments
-              position={[-2.2, 1.8, 0]} // Adjust position
-            >
-              MASAI
-              {/* Material for the text */}
-              <meshStandardMaterial color="white" />
+        font={fontJson}
+        size={1}
+        height={0.5}
+        bevelEnabled={true}
+        bevelThickness={0.1}
+        bevelSize={0.1}
+        bevelOffset={0}
+        bevelSegments={8}
+        position={[-2.2, 1.8, 0]}
+      >
+        MASAI
+        <meshStandardMaterial color="white" />
       </Text3D>
-
-        
-      {/* Create a starry background */}
       <Stars
-        radius={100} // Radius of the starry sphere
-        depth={50}   // Depth into which stars appear
-        count={5000} // Number of stars
-        factor={8}   // Star intensity and size factor
-        saturation={0} // Grayscale
-        fade // If you want a fading effect towards the edges
+        radius={100}
+        depth={50}
+        count={5000}
+        factor={8}
+        saturation={0}
+        fade
       />
-
-
-
-    <OrbitControls
-        enableZoom={true}   // Disable zooming
-        enablePan={false}    // Disable panning
-        maxPolarAngle={Math.PI / 2} // Limit vertical rotation to horizontal plane
-        minPolarAngle={Math.PI / 2} // Limit vertical rotation to horizontal plane
+      <OrbitControls
+        enableZoom={true}
+        enablePan={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
       />
-
-
     </>
   );
+};
 
-}
 
 function Hero() {
-
-
   const [kawhiDone, setKawhiDone] = useState(false);
 
   useEffect(() => {
@@ -166,42 +153,27 @@ function Hero() {
     }, 5400);
 
     return () => clearTimeout(timeoutId);
-  }, []); 
-
-
+  }, []);
 
   return (
     <>
-
-
-      { false && (
+      {false && (
         <div className='d-flex justify-content-center align-items-center mt-4'>
-            <img src={kawhi} height="500" alt="Kawhi" />
-        </div> 
-        )
-      } 
-
-      { true && 
-      (
-      <>
-      <div className='canvas-container extra-height'>
-        <Canvas style={{  width: '100%', height: '150%', backgroundColor: '#000033' }}>
-    
-          <Suspense fallback={null}>
-            <Scene />
-                    {/* Static text within the Canvas */}
-                                {/* Render 3D bubble text */}
-
-          </Suspense>
-        </Canvas> 
-
-
-        
-        <Card />
-      </div>
-      </>
-      )
-      }
+          <img src={kawhi} height="500" alt="Kawhi" />
+        </div>
+      )}
+      {true && (
+        <>
+          <div className='canvas-container extra-height'>
+            <Canvas style={{ width: '100%', height: '150%', backgroundColor: '#000033' }}>
+              <Suspense fallback={null}>
+                <Scene />
+              </Suspense>
+            </Canvas>
+            <Card />
+          </div>
+        </>
+      )}
     </>
   );
 }
